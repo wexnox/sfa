72341e2dfca1a13e2a528600f4dc20a01f3333ac
Lasse <lasseronnlie@hotmail.com>
2018-11-16 21:18:39 +0100

    migration table + seeding

--- database/migrations/2018_11_16_193403_create_products_table.php
+++ database/migrations/2018_11_16_193403_create_products_table.php
@@ -16,6 +16,23 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+//            $table->integer( 'category_id' )->unsigned();
+            $table->string('imagePath');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('pris');
+
+//            $table->double( 'original_price' );
+//            $table->double( 'discount_price' );
+//            $table->tinyInteger( 'in_stock' )->default( 1 );
+//            $table->tinyInteger( 'status' )->default( 0 );
+
+            $table->softDeletes();
+            // TODO: Det må legges til flere ting
+//            $table->foreign( 'category_id' )
+//                ->references( 'id' )->on( 'categories' )
+//                ->onDelete( 'cascade' )
+//                ->onUpdate( 'cascade' );
         });
     }
 

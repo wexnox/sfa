3b1926e42d386a0366fee476795291b0d3e70710
Lasse <lasseronnlie@hotmail.com>
2018-11-16 21:54:45 +0100

    migration table + seeding + controller + model

--- database/migrations/2018_11_16_193403_create_products_table.php
+++ database/migrations/2018_11_16_193403_create_products_table.php
@@ -20,14 +20,14 @@ class CreateProductsTable extends Migration
             $table->string('imagePath');
             $table->string('title');
             $table->text('description');
-            $table->integer('pris');
+            $table->integer('price');
 
 //            $table->double( 'original_price' );
 //            $table->double( 'discount_price' );
 //            $table->tinyInteger( 'in_stock' )->default( 1 );
 //            $table->tinyInteger( 'status' )->default( 0 );
 
-            $table->softDeletes();
+//            $table->softDeletes();
             // TODO: Det må legges til flere ting
 //            $table->foreign( 'category_id' )
 //                ->references( 'id' )->on( 'categories' )
--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -11,6 +11,34 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $product = new \App\Models\Product([
+            'imagePath'   => '...',
+            'title'       => 'Name',
+            'description' => 'Big name',
+            'price'       => 10
+        ]);
+        $product -> save ();
     }
+
+//        $categories = [ 'Hovedkort', 'Prosessor', 'Minnebrikker', 'Kontrollere', 'Skjermkort', 'Stromforsyning', 'Skjerm', 'Kabinett' ];
+//        foreach ( $categories as $category ) {
+//            # First fetch category Id
+//            $category = DB ::table ('categories') -> where ('name', '=', trim (strtolower ($category))) -> get ();
+//            $category_id = $category[0] -> id;
+//            $actual_price = rand (15.5, 100.5);
+//            $discount_factor = (10 / 100) * $actual_price; // 10% of actual price
+//            $discount_price = $actual_price - $discount_factor;
+//
+//            DB ::table ('products') -> insert ([
+//                'title'          => 'Product for ' . $category[0] -> name,
+//                'imagePath'      => 'https://www.komplett.no/img/p/800/c3e07913-3f53-4ef7-3879-1bba994c2209.jpg',
+//                'description'    => 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the',
+//                'category_id'    => $category_id,
+//                'original_price' => $actual_price,
+//                'discount_price' => $discount_price,
+//                'in_stock'       => 1,
+//                'status'         => 1,
+//                'created_at'     => Carbon ::now (),
+//            ]);
+//        }
 }

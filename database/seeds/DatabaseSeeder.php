63396a10ed21838e372546bb68735247addccbd8
wexnox <lasseronnlie@hotmail.com>
2018-12-16 17:15:13 +0100

    Merge branch 'master' of https://github.com/wexnox/sfa
    # Conflicts:
    #	routes/web.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-//            CategoriesTableSeeder::class,
-            ProductTableSeeder::class,
+            CategoriesTableSeeder::class
+//            ProductTableSeeder::class,
 
         ]);
     }

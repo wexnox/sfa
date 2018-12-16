59ddbc0c2e3729f03f0d95fa75b3fecd1f216d34
codeblueweb <hello@codeblue.no>
2018-12-16 23:42:42 +0100

    # Fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,6 +20,6 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class, 'id', 'category_id');
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 }
